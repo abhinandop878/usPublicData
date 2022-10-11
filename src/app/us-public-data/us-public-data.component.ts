17e8244182073cfffc826329a41218417036de84
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-11 20:48:25 +0530

    routes

--- src/app/us-public-data/us-public-data.component.ts
+++ src/app/us-public-data/us-public-data.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class UsPublicDataComponent implements OnInit {
 
   constructor() { }
-
+  
+  usPublicData={}
   ngOnInit(): void {
   }
 
